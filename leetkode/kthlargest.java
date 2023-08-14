15d074aaaec0946d2c9de9b822a9fb36f5a5023d
Swayams Bisoyi <swayamsbisoyi@gmail.com>
2023-08-14 21:56:41 +0530

    Create kthlargest.java

---
+++ leetkode/kthlargest.java
@@ -0,0 +1,14 @@
+// USE PRIORITY QUEUE , TRAVERSE TILL K-1 , RETURN Topmost (K)
+
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue <Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        for(int i = 0;i<nums.length;i++) pq.add(nums[i]);
+
+        for(int i = 0; i<k-1;i++) pq.remove();
+
+        return pq.peek();
+        
+    }
+}
