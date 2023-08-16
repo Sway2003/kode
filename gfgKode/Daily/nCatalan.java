80da222778895c629c16490b30308f4a725f02b2
Swayams Bisoyi <swayamsbisoyi@gmail.com>
2023-08-16 22:31:34 +0530

    Create nCatalan.java

---
+++ gfgKode/Daily/nCatalan.java
@@ -0,0 +1,20 @@
+
+
+class Solution {
+    public int findCatalan(int n) {
+        if (n == 0) {
+            return 1;
+        }      
+        final int MOD = (int) (1e9 + 7);
+        long[] v = new long[n + 1];
+        v[0] = v[1] = 1;
+        
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                v[i] = (v[i] + (v[j] * v[i - j - 1]) % MOD) % MOD;
+            }
+        }  
+        return (int) v[n];
+    }
+}
+        
